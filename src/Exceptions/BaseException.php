5814ab1bce0321278e08fd614a6158bf1dc14a59
qq1060656096 <1060656096@qq.com>
2018-08-09 10:57:40 +0800

    aaa: 1. 增加数据回调URL验证 2. 增加异常类 3. 增加单元测试h初始化文件

---
+++ src/Exceptions/BaseException.php
@@ -0,0 +1,6 @@
+<?php
+namespace Zwei\EnterpriseWechat\Exceptions;
+
+class BaseException extends \Exception
+{
+}
---
+++ src/Exceptions/DataCallbackUrlException.php
@@ -0,0 +1,15 @@
+<?php
+namespace Zwei\EnterpriseWechat\Exceptions;
+
+/**
+ * 数据回调URL
+ *
+ * @see 企业微信文档地址: https://work.weixin.qq.com/api/doc#12977/%E9%AA%8C%E8%AF%81URL%E6%9C%89%E6%95%88%E6%80%A7
+ *
+ * Class DataCallbackUrlException
+ * @package Zwei\EnterpriseWechat\Exceptions
+ */
+class DataCallbackUrlException extends BaseException
+{
+
+}
