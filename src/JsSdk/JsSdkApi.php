d64c90b5c466ec3130593cf6fd1df3bea6d5fb00
qq1060656096 <1060656096@qq.com>
2018-09-03 11:17:43 +0800

    aaa: 增加企业微信js sdk api get_jsapi_ticket

---
+++ src/JsSdk/JsSdkApi.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 赵思贵
+ * Date: 2018/9/3
+ * Time: 11:01
+ */
+
+namespace Zwei\WorkWechat\JsSdk;
+
+use Zwei\WorkWechat\ApiBase;
+
+/**
+ * js sdk api
+ *
+ * Class JsSdkApi
+ * @package Zwei\WorkWechat\JsSdk
+ */
+class JsSdkApi extends ApiBase implements JsSdkApiDefineInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function getJsApiTicket($accessToken)
+    {
+        $url = sprintf(self::URL_JS_API_TICKET, $accessToken);
+        $response = $this->client->request('get', $url, ['verify' => $this->sslVerify,]);
+        return $this->parseApiResult($response);
+    }
+
+}
---
+++ src/JsSdk/JsSdkApiDefineInterface.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 赵思贵
+ * Date: 2018/9/3
+ * Time: 11:01
+ */
+
+namespace Zwei\WorkWechat\JsSdk;
+
+/**
+ * js sdk api
+ *
+ * Interface JsSdkApiDefineInterface
+ * @package Zwei\WorkWechat\JsSdk
+ */
+interface JsSdkApiDefineInterface
+{
+    /**
+     * 获取企业的 jsapi_ticket
+     * 请求方式：GET（HTTPS）
+     * @var string
+     */
+    const URL_JS_API_TICKET = 'https://qyapi.weixin.qq.com/cgi-bin/get_jsapi_ticket?access_token=%s';
+
+    /**
+     * 获取企业的jsapi_ticket
+     *
+     * @param string $accessToken 企业access_token
+     * @return array
+     * @throws WorkWechatApiErrorCodeException 企业微信接口返回错误码异常
+     */
+    public function getJsApiTicket($accessToken);
+}
