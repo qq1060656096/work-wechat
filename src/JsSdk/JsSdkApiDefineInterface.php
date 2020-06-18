3123a5646d3d54f2855784b2d8172882e5ad74f7
zhao <zxeg.tp@foxmail.com>
2020-06-18 11:08:36 +0800

    增加接口
    增加接口

--- src/JsSdk/JsSdkApiDefineInterface.php
+++ src/JsSdk/JsSdkApiDefineInterface.php
@@ -22,6 +22,12 @@ interface JsSdkApiDefineInterface
      * @var string
      */
     const URL_JS_API_TICKET = 'https://qyapi.weixin.qq.com/cgi-bin/get_jsapi_ticket?access_token=%s';
+    /**
+     * 获取应用的 jsapi_ticket
+     * 请求方式：GET（HTTPS）
+     * @var string
+     */
+    const URL_JS_API_TICKET_AGENT_CONFIG = "https://qyapi.weixin.qq.com/cgi-bin/ticket/get?access_token=%s&type=agent_config";
 
     /**
      * 获取企业的jsapi_ticket
@@ -31,4 +37,13 @@ interface JsSdkApiDefineInterface
      * @throws WorkWechatApiErrorCodeException 企业微信接口返回错误码异常
      */
     public function getJsApiTicket($accessToken);
+    
+    /**
+     * 获取应用的jsapi_ticket
+     *
+     * @param string $accessToken 企业access_token
+     * @return array
+     * @throws WorkWechatApiErrorCodeException 企业微信接口返回错误码异常
+     */
+    public function getJsApiTicketAgentConfig($accessToken);
 }
